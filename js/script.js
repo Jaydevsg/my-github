f69916ef0ce579b719448296ec37f6c05c743dcc
Jayadev SG <94039893+Jaydevsg@users.noreply.github.com>
2022-01-11 18:08:02 +0530

    v2

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//select both the target with html elements
+const fetchDataBtn=document.querySelector('#fetchdata');
+const result=document.querySelector('#result');
+
+//Get the data from an api and set the contents of the result div accordingly
+async function getData(){
+result.innerHTML="Loading...";
+try{
+const res=await fetch('https://api.github.com/users/Jaydevsg')
+const jsonResult=await res.json();
+result.innerHTML=`
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">${jsonResult.name}</h5>
+<h6 class="card-subtitle mb-2 text-muted">${jsonResult.bio}</h6>
+<p class="card-text">${jsonResult.location}</p>
+<a href="${jsonResult.html_url}" class="card-link">Github Profile</a>
+</div>
+</div>
+`;
+}catch(error){
+    result.innerHTML=error;
+}
+}
+
+
+fetchDataBtn.addEventListener('click',getData);
